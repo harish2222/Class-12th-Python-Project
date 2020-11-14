89cb94ef011eaf5995a5cfe1ff6f6109063329d4
HARISHKALVA <harishkalva14gmail.com>
2020-11-15 02:06:32 +0530

    simple

--- menu.py
+++ menu.py
@@ -1,8 +1,9 @@
 from data_analyse import data_analyse_menu, maxima
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 import psycopg2 as pc
 from conn import *
+
 
 def wel():
     """
@@ -10,48 +11,41 @@ def wel():
     """
     print("|=====================================Welcome TO HK TRENDS======================================|")
 
+
 def maincode():
-    
     """
     Main menu is given here
     """
-    
-    db = dbconn("googletrends_data","postgres","postgresql")
+
+    db = dbconn("googletrends_data", "postgres", "postgresql")
     cur = db.cursor()
-    
-    
-    
-    print("\n\t\tEnter--1--Print raw data in console\n")                                                                          
-                                                                                                                     
+
+    print("\n\t\tEnter--1--Print raw data in console\n")
+
     print("\t\tEnter--2--Show Graphical Data\n")
 
-    print("\t\tEnter--3--Show DATA From Postgresql\n")                                                                          
-                                                                                                                     
-    print("\t\tEnter--4--Show Raw Data from Postgresql\n")                                                                      
-                                                                                                                     
-    print("\t\tEnter--5--Show DATA from CSV\n")                                                                                 
-                                                                                                                     
-    print("\t\tEnter--6--Data Analysis\n")                                                                                      
-                                                                                                                     
+    print("\t\tEnter--3--Show DATA From Postgresql\n")
+
+    print("\t\tEnter--4--Show Raw Data from Postgresql\n")
+
+    print("\t\tEnter--5--Show DATA from CSV\n")
+
+    print("\t\tEnter--6--Data Analysis\n")
+
     print("\t\tEnter--7--Exit\n")
-
-    
-
-
 
     usr_input = int(input("Enter the Number :\t"))
 
-    if usr_input==1:
+    if usr_input == 1:
         cur.execute("select * from programming_language.coding;")
         row = cur.fetchall()
         print(row)
         maincode()
 
-    elif usr_input==2:
+    elif usr_input == 2:
         data_visual()
 
-
-    elif usr_input==3:
+    elif usr_input == 3:
         df = dict()
         w = list()
         k = list()
@@ -63,7 +57,7 @@ def maincode():
         cur.execute("select * from programming_language.coding;")
         row = cur.fetchall()
         for r in row:
-            
+
             w.append(f"{r[0]}")
             k.append(f"{r[1]}")
             j.append(f"{r[2]}")
@@ -71,12 +65,13 @@ def maincode():
             c.append(f"{r[4]}")
             js.append(f"{r[5]}")
 
-        df = {"WEEK":w,"KOTLIN":k,"JAVA":j,"PYTHON":p,"C++":c,"JAVASCRIPT":js}
+        df = {"WEEK": w, "KOTLIN": k, "JAVA": j,
+              "PYTHON": p, "C++": c, "JAVASCRIPT": js}
         c = pd.DataFrame(df)
         print(c)
         maincode()
 
-    elif usr_input==4:
+    elif usr_input == 4:
         w = list()
         k = list()
         j = list()
@@ -87,7 +82,7 @@ def maincode():
         cur.execute("select * from programming_language.coding;")
         row = cur.fetchall()
         for r in row:
-            
+
             w.append(f"{r[0]}")
             k.append(f"{r[1]}")
             j.append(f"{r[2]}")
@@ -99,23 +94,25 @@ def maincode():
         t.append(j)
         t.append(p)
         t.append(c)
-        t.append(js)    
+        t.append(js)
         print(t)
         maincode()
 
-    elif usr_input==5:
-        csv_data = pd.read_csv("H:\PROGRAMMING\PYTHON\Class 12th Python Project\csv_data\kjpcjs.csv")
+    elif usr_input == 5:
+        csv_data = pd.read_csv(
+            "H:\PROGRAMMING\PYTHON\Class 12th Python Project\csv_data\kjpcjs.csv")
         print(csv_data)
         maincode()
-    elif usr_input==6:
+    elif usr_input == 6:
         data_analyse_menu()
         maincode()
 
-    elif usr_input==7:
+    elif usr_input == 7:
         exit()
 
-                
     db.close()
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     wel()
     maincode()

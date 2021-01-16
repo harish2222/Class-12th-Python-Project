d033b0919581c51faa411ab92e050b6c5a1004f7
HARISHKALVA <harishkalva14gmail.com>
2021-01-16 10:00:33 +0530

    small changes

--- mysqlcon.py
+++ mysqlcon.py
@@ -1,14 +1,17 @@
 import pymysql as pm
 
-def datacon(host,user,passwd,database):
+
+def datacon(host, user, passwd, database):
     """
     credentials for mysql database
     """
-    conn = pm.connect(host,user,passwd,database)
+    conn = pm.connect(host, user, passwd, database)
     cur = conn.cursor()
     cur.execute("SELECT * from google_trends;")
     r = cur.fetchall()
     for i in r:
         print(f"{i[0]}")
+
+
 if __name__ == "__main__":
-    datacon("localhost","root",'mysql','google_trends')
+    datacon("localhost", "root", 'mysql', 'google_trends')

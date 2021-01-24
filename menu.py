255d9beb2e66dd819dc8528c9189a5547c8f7843
HARISHKALVA <harishkalva14gmail.com>
2021-01-24 18:01:03 +0530

    changes

--- menu.py
+++ menu.py
@@ -1,8 +1,6 @@
 from conn import *
 from data_analyse import data_analyse_menu
-import pymysql as pms
-from mysqlcon import datacon
-import getpass
+
 import pandas as pd
 import pymysql as pm
 
@@ -27,15 +25,13 @@ def maincode(dbs, usr, pswd):
 
     cur = db.cursor()
 
-    print("\t\tEnter--1--Show DATA from CSV\n")
+    
 
-    print("\t\tEnter--2--Show DATA From Postgresql\n")
+    print("\t\tEnter--1--Show DATA From Postgresql\n")
 
-    print("\t\tEnter--3--Exit\n")
+    print("\t\tEnter--2--Exit\n")
 
     usr_input = int(input("Enter the Number :\t"))
-
-    
 
     if usr_input == 1:
 
@@ -64,64 +60,60 @@ def maincode(dbs, usr, pswd):
         maincode(datab, usr, pswd)
 
 
-    
-    elif usr_input == 2:
-        data_analyse_menu()
-        maincode(datab, usr, pswd)
 
-    elif usr_input == 3:
+
+    elif usr_input == 2:
         exit()
     db.close()
 
 
-# noinspection PyGlobalUndefined
-def mysql_runned_maincode(host, user, passwd, database):
-    
-    conn = pm.connect(host, user, passwd, database)
-    
-    cur = conn.cursor()
-
-    print("\t\n1.Mysql Raw Data")
-    print("\t\n2.Mysql Structure Data")
-    print("\t\n3.Exit")
-
-    usr_input = int(input("Enter the index:\t"))
-
-    if usr_input == 1:
-        query = str(input("Enter your query:\t"))
-        cur.execute(query)
-        row = cur.fetchall()
-        print(row)
-        mysql_runned_maincode(host, user, passwd, database)
-
-    elif usr_input == 2:
-        query = str(input("Enter The query:\t"))
-        cur.execute(query)
-        w = list()
-        k = list()
-        j = list()
-        p = list()
-        c = list()
-        js = list()
-        row = cur.fetchall()
-        for r in row:
-            w.append(f"{r[0]}")
-            k.append(f"{r[1]}")
-            j.append(f"{r[2]}")
-            p.append(f"{r[3]}")
-            c.append(f"{r[4]}")
-            js.append(f"{r[5]}")
-
-        df = {"WEEK": w, "KOTLIN": k, "JAVA": j,
-              "PYTHON": p, "C++": c, "JAVASCRIPT": js}
-        c = pd.DataFrame(df)
-        print(c)
-        mysql_runned_maincode(host, user, passwd, database)
-
-    if usr_input == 3:
-        exit()
-
-    conn.close()
+#
+# def mysql_runned_maincode(user, passwd, db):
+#     conn = pm.connect(host ='locahost', user=user, password=passwd, database=db)
+#
+#     cur = conn.cursor()
+#
+#     print("\t\n1.Mysql Raw Data")
+#     print("\t\n2.Mysql Structure Data")
+#     print("\t\n3.Exit")
+#
+#     usr_input = int(input("Enter the index:\t"))
+#
+#     if usr_input == 1:
+#         query = str(input("Enter your query:\t"))
+#         cur.execute(query)
+#         row = cur.fetchall()
+#         print(row)
+#         mysql_runned_maincode(host = "localhost", user, passwd, db)
+#
+#     elif usr_input == 2:
+#         query = str(input("Enter The query:\t"))
+#         cur.execute(query)
+#         w = list()
+#         k = list()
+#         j = list()
+#         p = list()
+#         c = list()
+#         js = list()
+#         row = cur.fetchall()
+#         for r in row:
+#             w.append(f"{r[0]}")
+#             k.append(f"{r[1]}")
+#             j.append(f"{r[2]}")
+#             p.append(f"{r[3]}")
+#             c.append(f"{r[4]}")
+#             js.append(f"{r[5]}")
+#
+#         df = {"WEEK": w, "KOTLIN": k, "JAVA": j,
+#               "PYTHON": p, "C++": c, "JAVASCRIPT": js}
+#         c = pd.DataFrame(df)
+#         print(c)
+#         mysql_runned_maincode(host = "localhost", user, passwd, db)
+#
+#     if usr_input == 3:
+#         exit()
+#
+#     conn.close()
 
 
 if __name__ == '__main__':
@@ -133,16 +125,15 @@ if __name__ == '__main__':
     print("\t5.Data Analysis\n")
     print("\t6.Exit\n")
     input_data = int(input("Enter the index of the databse Menu:\t"))
-    if input_data == 1:
-        host = str(input("Enter hosting address:\t"))
-        database = str(input("Enter the database name:\t"))
-        user = str(input("Enter user crendentials:\t"))
-        passwd = str(getpass.getpass("Enter user Password:\t"))
-        mysql_runned_maincode(host, user, passwd, database)
-    elif input_data == 2:
+    # if input_data == 1:
+    #     database = str(input("Enter the database name:\t"))
+    #     user = str(input("Enter user crendentials:\t"))
+    #     passwd = str(input("Enter user Password:\t"))
+    #     mysql_runned_maincode(host = "locahost",user=user, passwd=passwd, db=database)
+    if input_data == 2:
         datab = str(input("Enter the database name:\t"))
         usr = str(input("Enter user crendentials:\t"))
-        pswd = str(getpass.getpass("Enter user Password"))
+        pswd = str(input("Enter user Password"))
         wel()
         maincode(datab, usr, pswd)
     elif input_data == 3:
@@ -155,5 +146,4 @@ if __name__ == '__main__':
     elif input_data == 6:
         exit()
     else:
-        exit()        
-        
+        exit()
